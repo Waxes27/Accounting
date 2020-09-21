c8ebcc8ff49ea82a2611459fb389f5df08293d54
c4r8s3 <c4r8s3@c4r8s3.co.za>
2020-09-21 18:14:11 +0200

    acc app

--- __init__.py
+++
@@ -1,9 +0,0 @@
-import sys
-from user import authentication
-from transactions import journal
-from banking import reconciliation
-from banking.fvb import reconciliation as fvbrecon
-from banking.ubsa import reconciliation as ubsarecon
-from banking.online import reconciliation as onlinebanking
-import banking
-import requests
--- accounting.py
+++ accounting.py
@@ -1,5 +1,12 @@
-
-
+import sys
+from user import authentication
+from transactions import journal
+from banking import reconciliation
+from banking.fvb import reconciliation as fvbrecon
+from banking.ubsa import reconciliation as ubsarecon
+from banking.online import reconciliation as onlinebanking
+import banking
+import requests
 
 
 def run_app():
